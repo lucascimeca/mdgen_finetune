e3bc06a729ead333e560caf98a32824ca3908ace
lucascimeca <luca.scimeca@live.com>
2025-03-24 19:07:05 +0100

    functioning replay buffer

--- src/scripts/mdgen_launcher.sh
+++ src/scripts/mdgen_launcher.sh
@@ -5,7 +5,7 @@
 # passing in the different beta_start values you want.
 
 # If you need 5 distinct values of beta_start, define them here:
-beta_values=(1e6 1.0)
+beta_values=('rtb' 'tb')
 
 for beta in "${beta_values[@]}"; do
   echo "Submitting job with beta_start=${beta}"
