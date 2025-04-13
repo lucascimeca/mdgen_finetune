0c4d3c77312e65e1d30b7a93a65b4c768f13559e
lucascimeca <luca.scimeca@live.com>
2025-04-13 10:14:24 -0400

    fixed bugs

--- src/scripts/mdgen_launcher.sh
+++ src/scripts/mdgen_launcher.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-methods=('tb')
-betas=('1.' '1e6')
+methods=('tb', 'rtb')
+betas=('1.')
 replay_buffers=('False')
 vargrads=('True')
 
 for method in "${methods[@]}"; do
   # Set learning rate depending on the method
-  if [ "$method" == "rtb" ]; then
-    lr="6e-4"
-  else
-    lr="1e-5"
-  fi
+#  if [ "$method" == "rtb" ]; then
+  lr="6e-4"
+#  else
+#    lr="1e-5"
+#  fi
 
   for beta in "${betas[@]}"; do
     for vargrad in "${vargrads[@]}"; do
