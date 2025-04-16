76e9057bc277e4d88b59dd855e79c96e7efb93ea
lucascimeca <luca.scimeca@live.com>
2025-04-16 10:35:42 -0400

    fixed bugs

--- src/scripts/mdgen_finetune.sh
+++ src/scripts/mdgen_finetune.sh
@@ -50,7 +50,7 @@ python ../outsourced_train_posterior.py --epochs 5000 --traj_length 1000 --sampl
                                         --save_folder ~/scratch/mdgen/results/ --data_path ~/scratch/mdgen/data/ \
                                         --splits_path ../../splits/ --lora False --load_path ../../pretrained/ \
                                         --method $1 --learning_rate $2 --energy_temperature $3 \
-                                        --vargrad $4 --vargrad_sample_n0 4 --test_sample_size 100 \
+                                        --vargrad $4 --vargrad_sample_n0 4 --test_sample_size 50 \
                                         --load_outsourced_path ../../pretrained/mdgen_source_sampler.bin \
                                         --push_to_wandb True --resume True --load_outsourced_ckpt False \
                                         --rb_ratio .2 -rb $5
